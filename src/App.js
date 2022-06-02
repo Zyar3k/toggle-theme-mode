f3a87671d888bdc52e6b8cfa10c67f11327a11ae
Arkadiusz Zygan <a.zygan@gmail.com>
2022-06-02 14:12:48 +0200

    Toggle theme

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./styles/_main.scss";
+
+function App() {
+  const [theme, setTheme] = useState("light");
+  return (
+    <div className={theme}>
+      <div className="background flex">
+        <h1 className="text">Hello world! </h1>
+
+        <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+          Switch Theme
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
